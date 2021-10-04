83ce4390fc1957ae0ba602627f0e97980cf9915a
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2021-10-04 21:18:08 +0200

    added all exercises except 7

--- Ex-1/app/js/app.js
+++ Ex-1/app/js/app.js
@@ -1,16 +1,68 @@
 window.onload = function(){
     console.log("loaded");
 
-    document.getElementById('form').addEventListener('submit', e => {
+    let dishes = [ {
+        id: 1,
+        name: 'California Rolls',
+        price: 7.5
+    },
+    {
+        id: 2,
+        name: 'Paella',
+        price: 16.75
+    },
+    {
+        id: 3,
+        name: 'Cheeseburger',
+        price: 2.15
+    },
+    {
+        id: 4,
+        name: 'Takoyaki',
+        price: 10
+    },
+    {
+        id: 5,
+        name: 'Quatro Formaggio Pizza',
+        price: 6
+    }
+
+];
+
+
+    for (let dish of dishes) {
+        console.log(dish);
+
+        document.getElementById('dish-container').insertAdjacentHTML('beforeend', `<input type="checkbox" id="${dish.id}" name="dish" value="${dish.name}">
+        <label label for= "${dish.name}"> ${dish.name} </label> <br>`)
+    }
+
+    document.getElementById('submit-order').addEventListener('click', e => {
         e.preventDefault();
-        
-        let name = document.getElementById('name').value;
-        let email = document.getElementById('email').value;
-        let order = document.getElementById('order').value;
 
-        let message = `<p>The order for the customer ${name}  is the following:  ${order}. The customer may be notified by email:  ${email}</p>`
+        let customerName = document.getElementById('name').value;
+        let customerMail = document.getElementById('email').value;
 
-        document.getElementById("message").innerHTML = message;
+        let customerOrder = document.querySelector('input[name="dish"]:checked').value;
+
+        console.log(customerName, customerMail, customerOrder);
+
+        let printOrder = () => {
+            return `<p>The order for the customer ${customerName}  is the following:  ${customerOrder}. The customer may be notified by email:  ${customerMail}</p>`
+        };
+
+        document.getElementById("message").innerHTML = printOrder(customerName, customerMail, customerOrder);
+    })
+
+    document.getElementById('calculate-price').addEventListener('click', e => {
+        e.preventDefault();
+
+        let calculatePrice = () => {
+            return `The total price  will be ${dish.price}`;    
+        }
+
+        document.getElementById("message").innerHTML = calculatePrice();
+
     })
 }
 
